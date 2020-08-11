97f8d389d69e29fe1e76628ed6cdb6e420092c7a
yangdengcheng <dengcheng.yang@whats-egg.com>
2020-08-11 18:07:48 +0800

    push

--- week18/NOTE.md
+++ week18/NOTE.md
@@ -1,1 +1,17 @@
 # 每周总结可以写在这里
+
+#### Dev 工具
+
+- Server
+ - build: webpack babel vue jsx postcss
+ - watch: fsevent
+ - mock: mockjs
+ - http: ws
+
+- Client
+ - debugger: vscode devtool
+ - source map
+
+#### 单元测试
+
+在项目中需要对基础库和组件库做单元测试 保证整体基础代码的健壮性和稳定性
---
+++ week18/test-demo/test/add.test.js
@@ -0,0 +1,10 @@
+import add from '../src/add'
+var assert = require('assert');
+
+describe('add', function () {
+  describe('add', function () {
+    it('should return 7 when add(3,4)', function () {
+      assert.equal(add(3, 4), 7);
+    });
+  });
+});
